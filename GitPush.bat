8e4270d0e79ae5f2d4bd898591c003cf6033af54
xiechaoyi <xcy0609@qq.com>
2017-03-24 11:21:26 +0800

    migrate to 1.0.1
    Signed-off-by: xcy0609@qq.com

--- GitPush.bat
+++ GitPush.bat
@@ -1,5 +1,9 @@
 @echo off
-echo.请输入当前发布的版本号，如：1.0.0
-set /p push_version=
-git add -A && git commit -m "Version %push_version%" && git push -u origin master
-git tag -a %push_version% -m "By GitPush." && git push --tags
+set /p input=请输入当前发布的版本号（如:v160101）：
+if "%input%"=="" (
+    echo Auto Push
+    git add -A && git commit -a -m "GitPush" && git push -u origin master
+) else (
+    git add -A && git commit -a -m "Version %input%" && git push -u origin master
+    git tag -a %input% -m "By GitPush." && git push --tags -u origin master
+)
